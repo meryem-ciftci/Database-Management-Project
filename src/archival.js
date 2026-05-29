d245f0e2815112d6e42cfbd21bf9b9385860ab12
Damla Nur Kurt <damla.kurt8520@gmail.com>
2026-05-29 21:37:00 +0300

    archival güncellendi

--- src/archival.js
+++ src/archival.js
@@ -6,7 +6,7 @@ const titleInput = document.getElementById('title');
 const categoryInput = document.getElementById('category');
 
 let matchedResults = [];
-
+let inner = "";
 
 searchForm.addEventListener('submit', async function(event) {
     event.preventDefault();
@@ -28,6 +28,10 @@ searchForm.addEventListener('submit', async function(event) {
         const ISBN = ((bookList[bookList.length-1].ISBN).slice(0,5) + (Number.parseInt((bookList[bookList.length-1].ISBN.slice(5,21)))+1));
         await serverFonksiyonlari.addNewBook(ISBN, titleInput.value, categoryInput.value);  
     }
+    let add = (titleInput.valuetrim().padEnd(30, " ")) + (categoryInput.value.trim().padEnd(50, " "));
+    inner = inner + "<h4>" + add + "</h4>";
+    document.getElementById("id").innerHTML = inner;
+
 });
 
 
